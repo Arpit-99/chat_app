a2eca246a9b145c837557d90246c547a520c9f5e
arpit chandrakar <arpit.projectx@gmail.com>
2019-10-19 17:27:08 +0530

    Routing on app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
-import Chat from './Chat';
+import Chat from '../screens/Chat';
+import SignUp from '../screens/Signup';
+
+
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, authToken, ...rest }) => (
+    < Route {...rest} render={(props) =>
+        (authToken
+            ? <Component {...props} />
+            : <Redirect to='/signup' />
+        )} />
+);
+
+
+const Login = () => {
+    return <h2>Please login</h2>
+}
 
 class App extends React.Component {
+
+
     render() {
+
+        console.log('token', this.props.authToken);
         return (
-            <div>
-                <Chat />
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/signup" exact component={SignUp} />
+                    <Route path="/login" exact component={Login} />
+                    <PrivateRoute path="/chat" authToken={this.props.authToken} component={Chat} />
+                </div>
+            </BrowserRouter>
         );
     };
 
+
+
 };
-export default App;
+
+const mapStateToProps = (state) => {
+    return { authToken: state.auth.token }
+}
+
+export default connect(mapStateToProps)(App);
