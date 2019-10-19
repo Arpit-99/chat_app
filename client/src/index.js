13a947fa9b38ad043ecc9fe83a66a2dccaa91a90
arpit chandrakar <arpit.projectx@gmail.com>
2019-10-19 17:19:10 +0530

    new store config with local storage

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 ReactDOM.render(
-    <App />, document.querySelector('#root')
+
+    <Provider store={store} >
+        <App />
+    </Provider>
+    , document.querySelector('#root')
 );
